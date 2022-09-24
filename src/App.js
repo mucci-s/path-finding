4b5afac4a47ff6861c65dc9a018074ab9a217d5e
Mucci-s <muccis.sg@gmail.com>
2022-09-24 17:46:58 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import PathFinder from './PathFinder/PathFinder';
 function App() {
   return (
     <div className='App'>
-      <h2>ciaoooooooo</h2>
-      {/* <PathFinder></PathFinder> */}
+      
+      <PathFinder></PathFinder>
     </div>
   );
 }
